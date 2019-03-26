44e3ad28262348069ace5f4e59fdf1d31854721b
Noel Gárate <nouel3k@gmail.com>
2019-03-26 03:15:30 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,10 +1,4 @@
-exports.PORT = "3000";
-exports.DB_PATH = "mongodb://localhost:27017";
-exports.DB_NAME = "test";
-exports.ACCESS_TOKEN = "Bearer 18912abf26b841a0f0e98ee22f99ffeffdf";
-
-// Para subir a git
-// exports.PORT = "<listen port>";
-// exports.DB_PATH = "<mongodb path>";
-// exports.DB_NAME = "< mongo database name of your choice>";
-// exports.ACCESS_TOKEN = "<token literal>";
+exports.PORT = "<listen port>";
+exports.DB_PATH = "<mongodb path>";
+exports.DB_NAME = "< mongo database name of your choice>";
+exports.ACCESS_TOKEN = "<token literal>";
